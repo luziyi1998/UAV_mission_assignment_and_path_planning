ad9f9fc177dfd92eb7519ec150d67c3fb31d037e
luzy6 <252749799@qq.com>
2022-04-01 22:59:57 +0800

    实现astar搜索，astar冲突完成一半，

---
+++ MAPF/Astar.py
@@ -0,0 +1,143 @@
+import copy
+import heapq
+import numpy as np
+
+class My_PriorityQueue(object):
+    def __init__(self):
+        self._queue = []
+        self._index = 0
+
+    def push(self, item, priority):
+        """
+        队列由 (priority, index, item) 形式组成
+        priority 增加 "-" 号是因为 heappush 默认是最小堆
+        index 是为了当两个对象的优先级一致时，按照插入顺序排列
+        """
+        heapq.heappush(self._queue, (priority, self._index, item))
+        self._index += 1
+
+    def pop(self):
+        """
+        弹出优先级最高的对象
+        """
+        return heapq.heappop(self._queue)[-1]
+
+    def qsize(self):
+        return len(self._queue)
+
+    def empty(self):
+        return True if not self._queue else False
+
+
+class A(object):
+    def __init__(self, node_id, time_cost, flight_cost, path):
+        self.node_id = node_id
+        self.time_cost = time_cost
+        self.flight_cost = flight_cost
+        self.path = path
+    def __repr__(self):
+        return "{0} -- {1}".format(self.node_id, self.time_cost)
+
+
+
+
+def astar(transit_network, depot_id, package_id, Max_flight):
+    start_node = A(depot_id, 0, 0, [depot_id])
+    pq = My_PriorityQueue()
+    pq.push(start_node, start_node.time_cost + transit_network.edges[depot_id, package_id]['time'])
+#     max_count = 0
+    while not pq.empty():
+#         if pq.qsize() > max_count:
+#             max_count = pq.qsize()
+#             print(max_count)
+        cur = pq.pop()
+
+        if(cur.node_id == package_id):
+            # print("path", cur.path)
+            # print("flight_cost",cur.flight_cost)
+            return True, cur.path
+
+        for neighbor, w in transit_network[cur.node_id].items():
+            if cur.flight_cost + transit_network.edges[cur.node_id, neighbor]['weight'] < Max_flight:
+
+                path = copy.deepcopy(cur.path)
+                path.append(neighbor)
+
+                if neighbor == package_id:
+                    add_node = A(neighbor, cur.time_cost + transit_network.edges[cur.node_id, neighbor]['time'], cur.flight_cost + transit_network.edges[cur.node_id, neighbor]['weight'], path)
+                    pq.push(add_node, add_node.time_cost)
+                else:
+                    add_node = A(neighbor, cur.time_cost + transit_network.edges[cur.node_id, neighbor]['time'], cur.flight_cost + transit_network.edges[cur.node_id, neighbor]['weight'], path)
+                    pq.push(add_node, add_node.time_cost + transit_network.edges[neighbor, package_id]['time'])
+    return False, [], [0], -1
+
+
+def conflict_exit(T_interval, arrive_time, wait_time):
+    """
+    理论上，如果无人机在车来的前到达即可，也就是到达时间加上等待时间，是无人机最迟到换乘点的限制
+    :param T_interval:
+    :param arrive_time:
+    :param wait_time:
+    :return: extra wait time
+    """
+    if len(T_interval) == 0 or T_interval[0][0] > arrive_time + wait_time or T_interval[-1][1] < arrive_time + wait_time: # 该换乘点无占用  或在占用发生前或发生后该无人机已经可以离开，则不存在冲突
+        return -1
+    for i in range(len(T_interval)-1):
+        if T_interval[i][1] < arrive_time and T_interval[i+1][0] > arrive_time + wait_time: # 该无人机可以夹在换乘点占用中间的空闲进入且离开
+            return -1
+    return T_interval[-1][1] - arrive_time - wait_time
+
+
+# astar with coflicts avoid
+def astar_with_conflict_avoid(transit_network, transit_constarin, transit_edges, depot_id, package_id, Max_flight):
+    """
+    为了避免冲突，我们为每个无人机规划路径时，会将之前无人机在每个换乘点等待的时间戳保存下来，维护一个关于换乘点限制的字典，表示该换乘点都在哪一个时间戳区间内被占用，
+    后续无人机在做路径规划时，需要将换乘点约束考虑进来，即当路径规划到某一个换乘点的时候，应该将时间消耗的权重加上前面无人机所占用的时间，重新进行路径规划
+    :param transit_network:
+    :param transit_constarin:
+    :param depot_id:
+    :param package_id:
+    :param Max_flight:
+    :return: bool, route
+    """
+    wait_times = 0.2
+    transit_wait = {}
+    #更新当前无人机对每个传输点可能的等待时机
+    for edge in transit_edges:
+        transit_wait[(edge[0], edge[1])] = wait_times * np.random.rand() * transit_network.edges[edge]['time']
+        transit_network.edges[(edge[0], edge[1])]['time'] += transit_wait[(edge[0], edge[1])]
+
+    start_node = A(depot_id, 0, 0, [depot_id])
+    pq = My_PriorityQueue()
+    pq.push(start_node, start_node.time_cost + transit_network.edges[depot_id, package_id]['time'])
+    while not pq.empty():
+#         if pq.qsize() > max_count:
+#             max_count = pq.qsize()
+#             print(max_count)
+        cur = pq.pop()
+
+        if(cur.node_id == package_id):
+            # print("path", cur.path)
+            # print("flight_cost",cur.flight_cost)
+            return True, cur.path, transit_wait
+
+        for neighbor, w in transit_network[cur.node_id].items():
+            if cur.flight_cost + transit_network.edges[cur.node_id, neighbor]['weight'] < Max_flight:
+
+                if transit_network.edges[cur.node_id, neighbor]['type'] == 'transit':
+                    # 当前无人机到达该站点的时间加上无人机在该站点等待的时间后得到的时间戳，存在冲突，那么，无人机仍然想走原路，则到达时间延长，则等待时间加长
+                    extra_wait = conflict_exit(transit_constarin[cur.node_id], cur.time_cost, transit_wait[(cur.node_id, neighbor)])
+                    if extra_wait != -1: # exit conflict
+                        transit_wait[cur.node_id, neighbor] += extra_wait
+
+
+                path = copy.deepcopy(cur.path)
+                path.append(neighbor)
+
+                if neighbor == package_id:
+                    add_node = A(neighbor, cur.time_cost + transit_network.edges[cur.node_id, neighbor]['time'] + transit_wait[(cur.node_id, neighbor)], cur.flight_cost + transit_network.edges[cur.node_id, neighbor]['weight'], path)
+                    pq.push(add_node, add_node.time_cost)
+                else:
+                    add_node = A(neighbor, cur.time_cost + transit_network.edges[cur.node_id, neighbor]['time'] + transit_wait[(cur.node_id, neighbor)], cur.flight_cost + transit_network.edges[cur.node_id, neighbor]['weight'], path)
+                    pq.push(add_node, add_node.time_cost + transit_network.edges[neighbor, package_id]['time'])
+    return False, [], [0], -1
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import MAPF.single_drone_FP
 import numpy as np
 import networkx as nx
 from math import radians, cos, sin, asin, sqrt
-
+import MAPF.Astar
 
 def haversine(lon1, lat1, lon2, lat2):  # 经度1，纬度1，经度2，纬度2 （十进制度数）
     """
@@ -24,8 +24,7 @@ def haversine(lon1, lat1, lon2, lat2):  # 经度1，纬度1，经度2，纬度2 
     return c * r * 1000
 
 
-
-def UAV_task_allocation_path_planning(N_depots, N_transit_edges, N_packages, Max_flight):
+def UAV_task_allocation_path_planning(N_depots, N_transit_edges, N_packages, N_drones, Max_flight):
     # get a graph that randomly generate depot nodes, transit nodes and package nodes and cost_matrix
     TG, depots_node, transit_node, packages_node, transit_edges, cost_matrix = graph.generate_graph.return_Digraph(N_depots,
                                                                                                 N_transit_edges, N_packages)
@@ -114,6 +113,7 @@ def UAV_task_allocation_path_planning(N_depots, N_transit_edges, N_packages, Max
     drone_one_dilivery_route = []
     drone_one_dilivery_time_stamp = []
     drone_one_dilivery_time = []
+    fail_packages = []
     # now we compute each drone routes(我们应该为每个无人机先计算出一个交付任务——从仓库出发再到回到仓库，然后计算这些交付序列的冲突节点,解决所有冲突后，得到所有无人机该交付任务的最终路径)
     # 我们现在只算每个无人机第一个任务路径
     for i in range(len(drone_tours)):
@@ -125,12 +125,14 @@ def UAV_task_allocation_path_planning(N_depots, N_transit_edges, N_packages, Max
             drone_one_dilivery_time_stamp.append(departure_time_stamp)
             drone_one_dilivery_time.append(departure_time)
         else:
+            if package in packages_node:
+                fail_packages.append(package)
             drone_one_dilivery_route.append([])
             drone_one_dilivery_time_stamp.append([0])
             drone_one_dilivery_time.append(-1) # denote fail
 
 
-    return drone_one_dilivery_route, drone_one_dilivery_time_stamp, drone_one_dilivery_time
+    return drone_one_dilivery_route, drone_one_dilivery_time_stamp, drone_one_dilivery_time, fail_packages
 
 
 
@@ -181,7 +183,136 @@ def one_drone_dilivery_departure_route(TG, transit_node, transit_edges, depot_id
     return True, sub_mission_route_by_LP, time_stamp, route_plan_time_cost
 
 
+def one_drone_dilivery_departure_route_by_astar(TG, transit_node, transit_edges, depot_id, package_id, Max_flight):
+    # now we get a specific transit network only contain one depot and one package and transit node
+    transit_network = MAPF.single_drone_FP.generate_transit_network(TG, transit_node, transit_edges, depot_id, package_id)
 
+    success, sub_mission_way = MAPF.Astar.astar(transit_network, depot_id, package_id, Max_flight)
+    if success == False:
+        return False, [], [0], -1
+    # path finding by astar
+    route_plan_distance_cost = 0
+    route_plan_time_cost = 0
+    time_stamp = [0.0]
+    for (i, j) in zip(sub_mission_way[:len(sub_mission_way)-1], sub_mission_way[1:]):
+        route_plan_distance_cost += transit_network.edges[i, j]['weight']
+        route_plan_time_cost += transit_network.edges[i, j]['time']
+        time_stamp.append(route_plan_time_cost)
+    print(depot_id, " to", package_id, "route_plan_distance_cost", route_plan_distance_cost)
+    print("route:", sub_mission_way)
+    print("use time", route_plan_time_cost)
+
+    return True, sub_mission_way, time_stamp, route_plan_time_cost
+
+
+def UAV_task_allocation_path_planning_by_astar(N_depots, N_transit_edges, N_packages, N_drones, Max_flight):
+    # get a graph that randomly generate depot nodes, transit nodes and package nodes and cost_matrix
+    TG, depots_node, transit_node, packages_node, transit_edges, cost_matrix = graph.generate_graph.return_Digraph(N_depots,
+                                                                                                N_transit_edges, N_packages)
+
+    def dist(a, b, TG=TG):
+        lat1 = TG.nodes[a]['lat']
+        lon1 = TG.nodes[a]['lon']
+        lat2 = TG.nodes[b]['lat']
+        lon2 = TG.nodes[b]['lon']
+        #     print("node",a,lon1,lat1,"to node",b,lon2,lat2," liner distance:",haversine(lon1, lat1, lon2, lat2))
+        #     print(haversine(lon1, lat1, lon2, lat2))
+        #     print(lat1, lon1, lat2, lon2)
+        return haversine(lon1, lat1, lon2, lat2)
+
+
+    # update other parameters
+    total_nodes = len(TG.nodes)
+    N_transits = len(transit_node)
+
+    # we get transit node, package node and depot node, now we store the rest nodes are normal node
+    normal_node = np.array([])
+    for i in range(len(TG.nodes())):
+        if i not in depots_node and i not in transit_node and i not in packages_node:
+            normal_node = np.append(normal_node, i)
+
+    print("do task allocation")
+    drone_tours = task_allocation_layer.task_allocation.task_allocation(total_nodes, N_depots, N_packages, N_drones,
+                                                                        cost_matrix, depots_node, transit_node,
+                                                                        packages_node, normal_node)
+
+
+    # find each drones route
+    drone_routes = []
+    for i in range(len(drone_tours)):
+        cur_route = [drone_tours[i][0]]
+        #     print("drone", i, "route:")
+        for (node1, node2) in zip(drone_tours[i][:len(drone_tours[i]) - 1], drone_tours[i][1:]):
+            cur_route.extend(nx.astar_path(TG, node1, node2, dist)[1:])
+        #         print(nx.astar_path(TG, node1, node2, dist))
+        drone_routes.append(cur_route)
+
+    pos_location = {}
+    for i in TG.nodes:
+        pos_location[i] = (TG.nodes[i]['lon'], TG.nodes[i]['lat'])
+
+    node_size = 1000
+    colo_map = {}
+    nod_size_map = {}
+    for i in transit_node:
+        colo_map[i] = 'green'
+        nod_size_map[i] = node_size
+    for i in packages_node:
+        colo_map[i] = 'red'
+        nod_size_map[i] = node_size
+    for i in depots_node:
+        colo_map[i] = 'black'
+        nod_size_map[i] = node_size
+    for i in normal_node:
+        colo_map[i] = 'grey'
+        nod_size_map[i] = 0
+    len(colo_map)
+    color_map = []
+    node_size_map = []
+    for i in range(len(colo_map)):
+        color_map.append(colo_map[i])
+        node_size_map.append(nod_size_map[i])
+
+    import matplotlib.pyplot as plt
+    plt.rcParams['figure.figsize'] = (40, 40)  # 单位是inches
+    # green transit_node
+    # black depot_node
+    # red package node
+    # grey normal_node
+    nx.draw(TG,
+            pos=pos_location,
+            node_size=node_size_map,
+            cmap=plt.get_cmap('viridis'),
+            node_color=color_map,
+            width=1,
+            with_labels=True,
+            font_color='white')
+    fig_name = 'bicycle_map.jpg'
+    plt.savefig(fig_name, dpi=200)
+    # plt.show()
+
+    drone_one_dilivery_route = []
+    drone_one_dilivery_time_stamp = []
+    drone_one_dilivery_time = []
+    fail_packages = []
+    # now we compute each drone routes(我们应该为每个无人机先计算出一个交付任务——从仓库出发再到回到仓库，然后计算这些交付序列的冲突节点,解决所有冲突后，得到所有无人机该交付任务的最终路径)
+    # 我们现在只算每个无人机第一个任务路径
+    for i in range(len(drone_tours)):
+        start_depot, package = drone_tours[i][0:2]
+        success, departure_route, departure_time_stamp, departure_time = one_drone_dilivery_departure_route_by_astar(TG, transit_node, transit_edges, start_depot, package, Max_flight)
+        if success:
+            drone_one_dilivery_route.append(departure_route)
+            drone_one_dilivery_time_stamp.append(departure_time_stamp)
+            drone_one_dilivery_time.append(departure_time)
+        else:
+            if package in packages_node:
+                fail_packages.append(package)
+            drone_one_dilivery_route.append([])
+            drone_one_dilivery_time_stamp.append([0])
+            drone_one_dilivery_time.append(-1) # denote fail
+
+
+    return drone_one_dilivery_route, drone_one_dilivery_time_stamp, drone_one_dilivery_time, fail_packages
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -190,6 +321,7 @@ if __name__ == '__main__':
     parser.add_argument("--N_packages", type=int, default=60, help="number of packages")
     parser.add_argument("--N_transit_edges", type=int, default=20, help="number of transit edges")
     parser.add_argument("--Max_flight", type=int, default=400, help="drone's max flight distance")
+    parser.add_argument("--Algorithm", type=str, default='astar', help="drone's max flight distance")
     # 接受基础参数
     opt = parser.parse_args()
     N_depots = opt.N_depots
@@ -197,15 +329,22 @@ if __name__ == '__main__':
     N_packages = opt.N_packages
     N_transit_edges = opt.N_transit_edges
     Max_flight = opt.Max_flight
+    Algorithm = opt.Algorithm
     # print(N_depots, N_drones, N_packages, N_transit_edges)
 
-    drone_one_dilivery_route, drone_one_delivery_time_stamp, drone_one_dilivery_time = UAV_task_allocation_path_planning(N_depots, N_transit_edges, N_packages, Max_flight)
+    if Algorithm == 'astar':
+        print("use astar")
+        drone_one_dilivery_route, drone_one_delivery_time_stamp, drone_one_dilivery_time, fail_packages = UAV_task_allocation_path_planning_by_astar(N_depots, N_transit_edges, N_packages, N_drones, Max_flight)
+    else:
+        drone_one_dilivery_route, drone_one_delivery_time_stamp, drone_one_dilivery_time, fail_packages = UAV_task_allocation_path_planning(N_depots, N_transit_edges, N_packages, N_drones, Max_flight)
 
 
     for (route, time_stamp, time) in zip(drone_one_dilivery_route, drone_one_delivery_time_stamp, drone_one_dilivery_time):
         print("route:", route)
         print("time_stamp", time_stamp)
         print("time", time)
+    print("fail deliveries", fail_packages)
+
 
 
 
